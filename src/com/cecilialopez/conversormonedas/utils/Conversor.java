0010c935708602d84c11d266372f3c05cb46486c
Cecilia Lopez <andreacecilopez88@gmail.com>
2025-06-11 18:25:08 -0300

    add Conversor class for print menu

---
+++ src/com/cecilialopez/conversormonedas/utils/Conversor.java
@@ -0,0 +1,21 @@
+package com.cecilialopez.conversormonedas.utils;
+
+public class Conversor {
+    public static void exibirMenu(){
+        System.out.println("""
+                ************************************************
+                Sea bienvenido al Conversor de Moneda =]
+                
+                1) Dólar =>> Peso argentino
+                2) Peso argentino =>> Dólar
+                3) Dólar =>> Real brasileño
+                4) Real brasileño =>> Dólar
+                5) Dólar =>> Peso colombiano
+                6) Peso colombiano =>> Dolar
+                7) Salir
+                
+                Elija una opción válida:
+                ************************************************
+                """);
+    }
+}
