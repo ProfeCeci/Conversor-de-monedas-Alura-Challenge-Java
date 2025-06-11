3860bc048086e12c50e257c1d22fbba7feeec0cf
Cecilia Lopez <andreacecilopez88@gmail.com>
2025-06-11 18:18:39 -0300

    Add CambioMoneda Record Class

---
+++ src/com/cecilialopez/conversormonedas/models/CambioMoneda.java
@@ -0,0 +1,9 @@
+package com.cecilialopez.conversormonedas.models;
+
+import java.util.Map;
+
+public record CambioMoneda(
+        String base_code,
+        Map<String, Double> conversion_rates
+) {
+}
